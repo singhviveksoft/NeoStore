647b503dc9704768dcd222c584b41733c5552258
vivek <singh.vivek@neoftmail.com>
2021-06-13 01:11:39 +0530

    working on edit my cart item quantity

---
+++ app/src/main/java/com/example/neosoftassignmentproject/model/Deleteitem.kt
@@ -0,0 +1,9 @@
+package com.example.neosoftassignmentproject.model
+
+data class Deleteitem(
+    val `data`: Boolean,
+    val message: String,
+    val status: Int,
+    val total_carts: Int,
+    val user_msg: String
+)
